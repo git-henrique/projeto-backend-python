88e8d1046702bf27536b9bda4e9bbdf9943206be
git-henrique <henriqueoliveirag@alu.ufc.br>
2025-11-21 15:23:42 -0300

    UPDATE: algumas correções e tela da API funcionando

--- app/database.py
+++ app/database.py
@@ -7,18 +7,11 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# Configuração especial para SQLite (permite múltiplas conexões com FastAPI)
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-#dependencia para a FastAPI usar
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-        
