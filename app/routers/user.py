def24c93aa94bcbda8c3077caebd61bb08dbc0fe
henriqueoliveiragsdev <pedhenriqueogomes@gmail.com>
2025-12-20 20:33:14 -0300

    FEAT: inclusao do user

---
+++ app/routers/user.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPExeception
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal
+from app.schemas.user import UserCreate, UserResponse
+from app.crud.user import create_user, get_user_by_email
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/register", response_model=UserResponse)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    if get_user_by_email(db, user.email):
+        raise HTTPExeception(status_code=400, detail="Email já cadastrado")
+    
+
+    return create_user(db, user)
