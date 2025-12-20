97dbffc59318802fe4359a7b58153ab138f90cfc
henriqueoliveiragsdev <pedhenriqueogomes@gmail.com>
2025-12-20 19:05:40 -0300

    FIX: alteração no dado 'senha' do user

--- app/models/user.py
+++ app/models/user.py
@@ -7,6 +7,5 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
-    role = Column(String, nullable=False) # 'cliente' ou 'prestador'
-     
+    hashed_password = Column(String, nullable=False)
+    role = Column(String, nullable=False)  # 'cliente' ou 'prestador'
